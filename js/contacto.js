b4f79d0e96d6b7ea1f9945091b28d82f3179ccc0
lengstfeld <lengstfeld@gmail.com>
2024-05-29 00:39:34 -0300

    Se tuvieron que cambiar el lugar donde se llamaba al js de los formularios ya que no iniciavan al hacer click

--- js/contacto.js
+++ js/contacto.js
@@ -1,35 +1,30 @@
-export function enviarFormulario() {
+function enviarFormulario() {
   const email = document.getElementById("email").value.trim();
-  const asunto = document.getElementById("asunto");
+  const asunto = document.getElementById("asunto").value;
   const mensaje = document.getElementById("mensaje").value.trim();
 
-  let valid = true;
   let err = [];
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   if (email === "") {
     err.push("* Debe colocar un email. \n");
-    valid = false;
-  } else if (!emailPattern.test(email.value)) {
+  } else if (!emailPattern.test(email)) {
     err.push("* Email no valido. \n");
-    valid = false;
   }
 
-  if (asunto.value === "") {
+  if (asunto === "") {
     err.push("* Debe de seleccionar un asunto. \n");
-    valid = false;
   }
 
   if (mensaje === "") {
     err.push("* Debe de colocar un mensaje. \n");
-    valid = false;
   }
 
-  if (valid) {
+  if (err.length > 0) {    
+    alert(err);
+  } else{    
     alert("Tu mensaje fue enviado correctamente.");
     document.getElementById("formularioContacto").reset();
-  } else if (valid && err.length > 0) {
-    alert(err);
   }
 
   return false;
